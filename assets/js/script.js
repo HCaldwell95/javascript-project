c302333c42dfbf7caa5bed8219b37ef47c597b05
Hayden Caldwell <hade_caldwell@msn.com>
2023-12-12 22:42:57 +0000

    Add compare function with first user choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
 // Variables to store user and computer choices
 var userChoice;
 var computerChoice;
+
+// Compares user and computer choices to determine the winner
+function compare(comChoice) {
+    // Checks for a draw
+    if (userChoice === comChoice) {
+        return "The computer also chose " + computerChoice + ":" + "<br>" + "the result is a draw! :|";
+    
+    // Checks outcome if user chooses candy cane
+    } else if (userChoice === "candy cane") {
+        if (comChoice === "star") {
+            return "star eradicates candy cane <br> You Lose!";
+        } else if (comChoice === "bauble") {
+            return "candy cane bursts bauble <br> You Win!";
+        } else if (comChoice === "gingerbread") {
+            return "candy cane destroys <br> You Win!";
+        } else if (comChoice === "jingle") {
+            return "jingle shatters candy cane <br> You Lose!";
+        }
+
+    }
+
+
+
+
+
+
+}
