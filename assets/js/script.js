4c5a6f131d2cf1fe84d9e7f1a169bdd887126f4f
Hayden Caldwell <hade_caldwell@msn.com>
2023-12-13 00:28:22 +0000

    Add jsQuery script to index.html and amended script.js ordering

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,44 @@
 var userChoice;
 var computerChoice;
 
+/* Defines rules for each combination */
+var rules = {
+    "candy cane": {
+        "star": "star eradicates candy cane <br> You Lose!",
+        "jingle": "jingle shatters candy cane <br> You Lose!",
+        "bauble": "candy cane bursts bauble <br> You Win!",
+        "gingerbread": "candy cane obliterates gingerbread <br> You Win!"
+    },
+
+    "bauble": {
+        "candy cane": "candy cane bursts bauble <br> You Lose!",
+        "gingerbread": "gingerbread smothers bauble <br> You Lose!",
+        "jingle": "bauble isolates jingle <br> You Win!",
+        "star": "bauble captures star <br> You Win!"
+    },
+
+    "star": {
+        "bauble": "bauble captures star <br> You Lose!",
+        "jingle": "jingle outshines star <br> You Lose!",
+        "candy cane": "star eradicates candy cane <br> You Win!",
+        "gingerbread": "star evaporates gingerbread <br> You Win!"
+    },
+
+    "gingerbread": {
+        "star": "star evaporates gingerbread <br> You Lose!",
+        "candy cane": "candy cane obliterates gingerbread <br> You Lose!",
+        "bauble": "gingerbread smothers bauble <br> You Win!",
+        "jingle": "gingerbread consumes jingle <br> You Win!"
+    },
+
+    "jingle": {
+        "bauble": "bauble isolates jingle <br> You Lose!",
+        "gingerbread": "gingerbread consumes jingle <br> You Lose!",
+        "candy cane": "jingle shatters candy cane <br> You Win!",
+        "star": "jingle outshines star <br> You Win!"
+    },
+};
+
 /* Compares user and computer choices to determine the winner */
 function compare(comChoice) {
     /* Checks for a draw */
@@ -9,44 +47,10 @@ function compare(comChoice) {
         return "The computer also chose " + computerChoice + ":" + "<br>" + "the result is a draw! :|";
     }
 
-    /* Defines rules for each combination */
-    var rules = {
-        "candy cane": {
-            "star": "star eradicates candy cane <br> You Lose!",
-            "jingle": "jingle shatters candy cane <br> You Lose!",
-            "bauble": "candy cane bursts bauble <br> You Win!",
-            "gingerbread": "candy cane obliterates gingerbread <br> You Win!"
-        },
-
-        "bauble": {
-            "candy cane": "candy cane bursts bauble <br> You Lose!",
-            "gingerbread": "gingerbread smothers bauble <br> You Lose!",
-            "jingle": "bauble isolates jingle <br> You Win!",
-            "star": "bauble captures star <br> You Win!"
-        },
-
-        "star": {
-            "bauble": "bauble captures star <br> You Lose!",
-            "jingle": "jingle outshines star <br> You Lose!",
-            "candy cane": "star eradicates candy cane <br> You Win!",
-            "gingerbread": "star evaporates gingerbread <br> You Win!"
-        },
-
-        "gingerbread": {
-            "star": "star evaporates gingerbread <br> You Lose!",
-            "candy cane": "candy cane obliterates gingerbread <br> You Lose!",
-            "bauble": "gingerbread smothers bauble <br> You Win!",
-            "jingle": "gingerbread consumes jingle <br> You Win!"
-        },
-
-        "jingle": {
-            "bauble": "bauble isolates jingle <br> You Lose!",
-            "gingerbread": "gingerbread consumes jingle <br> You Lose!",
-            "candy cane": "jingle shatters candy cane <br> You Win!",
-            "star": "jingle outshines star <br> You Win!"
-        },
-    }
-};
+    /* Use the rules variable to determine the outcome */
+    var outcome = rules[userChoice][comChoice];
+    return outcome;
+}
 
 /* jQuery function to be executed when the document is ready */
 $(document).ready(function(){
@@ -76,7 +80,7 @@ $(document).ready(function(){
         /* Display the result in the HTML element with class "result" */
         $(".result").html("<h1>Result:</h1><p>User: " + userChoice.toUpperCase() + "<br>" + 
                             "Computer: " + computerChoice.toUpperCase() + "</p>" + "<p>" + result + "</p>");
-    })
+    });
 });
 
 
